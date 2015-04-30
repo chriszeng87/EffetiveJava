ecd86ae854c1e58c6daf30cc9dc512736503108b
chriszeng87 <chrisgardner87@gmail.com>
2015-04-30 12:09:15 +0800

    add SingleInstance

---
+++ src/Elvis.java
@@ -0,0 +1,9 @@
+
+public enum Elvis {
+
+	INSTANCE;
+	
+	public void leaveTheBuilding() {
+		
+	}
+}
---
+++ src/ElvisMain.java
@@ -0,0 +1,12 @@
+
+public class ElvisMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Elvis.INSTANCE.leaveTheBuilding();
+
+	}
+
+}
